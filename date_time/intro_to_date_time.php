7748fc09cdcfa585f72f8e081472d246976bc69c
adrian.ayroso <adrian.ayroso@telusinternational.com>
2017-09-05 06:33:52 +0800

    DateTime lecture.

--- date_time/intro_to_date_time.php
+++ date_time/intro_to_date_time.php
@@ -1,8 +1,26 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+//Create a DateTime object
+    $date  = new DateTime;
+    var_dump($date);
+    
+//Format a DateTime content
+    echo "<br/>";
+    echo $date->format('dS M Y');
+     
+// Format a DateTime with a time-stamp (unix format)
+    echo "<br/>";
+    echo $date->getTimestamp();
+    
+// Using the Scope Resolution Operator
+    $dateString = '2017-11-16';
+    $date = DateTime::createFromFormat('Y-m-d',$dateString);
+    var_dump($date);
+    
+ // Set the Date and Time
+    $dateObj = new DateTime;
+    
+    $dateObj->setDate(2017,09,04);
+    $dateObj->setTime(12,30,30);
+    var_dump($dateObj);
+    
